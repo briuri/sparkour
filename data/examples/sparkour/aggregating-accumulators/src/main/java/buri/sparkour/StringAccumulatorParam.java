b5b5e4a301f7bed927734745aa2c6b8fe345266a
briuri <brian@urizone.net>
2016-05-07 22:43:18 +0000

    SPARKOUR-15: Whitespace fixing.

--- data/examples/sparkour/aggregating-accumulators/src/main/java/buri/sparkour/StringAccumulatorParam.java
+++ data/examples/sparkour/aggregating-accumulators/src/main/java/buri/sparkour/StringAccumulatorParam.java
@@ -6,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *	http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,20 +20,20 @@ package buri.sparkour;
 import org.apache.spark.AccumulatorParam;
 
 /**
- * A custom accumulator for string concatenation
- * Contrived example -- see recipe for caveats.
+ * A custom accumulator for string concatenation Contrived example -- see recipe
+ * for caveats.
  */
 public class StringAccumulatorParam implements AccumulatorParam<String> {
 
-    public String zero(String initialValue) {
-        return (initialValue);
-    }
+	public String zero(String initialValue) {
+		return (initialValue);
+	}
 
-    public String addInPlace(String s1, String s2) {
-        return (s1.trim() + " " + s2.trim());
-    }
+	public String addInPlace(String s1, String s2) {
+		return (s1.trim() + " " + s2.trim());
+	}
 
-    public String addAccumulator(String s1, String s2) {
-        return (addInPlace(s1, s2));
-    }
+	public String addAccumulator(String s1, String s2) {
+		return (addInPlace(s1, s2));
+	}
 }
