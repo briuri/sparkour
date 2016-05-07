828f4b2efd6f2cc637c9f69ca9d74633f2cb436b
buri <buri@c041af27-2689-401b-8632-fcab6d716d80>
2016-05-07 22:41:34 +0000

    SPARKOUR-15: Initial checkin of example source code.

---
+++ trunk/data/examples/sparkour/aggregating-accumulators/src/main/java/buri/sparkour/JAggregatingAccumulators.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package buri.sparkour;
+
+import org.apache.spark.Accumulator;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SQLContext;
+
+
+/**
+ * Uses accumulators to provide statistics on potentially incorrect data.
+ */
+public final class JAggregatingAccumulators {
+
+	public static void main(String[] args) throws Exception {
+		SparkConf sparkConf = new SparkConf().setAppName("JAggregatingAccumulators");
+		JavaSparkContext sc = new JavaSparkContext(sparkConf);
+                SQLContext sqlContext = new SQLContext(sc);
+
+                // Create an accumulator to count how many rows might be inaccurate.
+                Accumulator<Integer> heightCount = sc.accumulator(0);
+
+                // Create an accumulator to store all questionable values.
+                Accumulator<String> heightValues = sc.accumulator("", new StringAccumulatorParam());
+
+                // A function that checks for questionable values
+                VoidFunction<Row> validate = new VoidFunction<Row>() {
+                    public void call(Row row) {
+                        Long height = row.getLong(row.fieldIndex("height"));
+                        if (height < 15 || height > 84) {
+                            heightCount.add(1);
+                            heightValues.add(String.valueOf(height));
+                        }
+                    }
+                };
+
+                // Create a DataFrame from a file of names and heights in inches.
+                DataFrame heightDF = sqlContext.read().json("heights.json");
+
+                // Validate the data with the function.
+                heightDF.javaRDD().foreach(validate);
+                
+                // Show how many questionable values were found and what they were.
+                System.out.println(String.format("%d rows had questionable values.", heightCount.value()));
+                System.out.println(String.format("Questionable values: %s", heightValues.value()));
+                
+		sc.stop();
+	}
+}
---
+++ trunk/data/examples/sparkour/aggregating-accumulators/src/main/scala/buri/sparkour/StringAccumulatorParam.scala
@@ -0,0 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// scalastyle:off println
+package buri.sparkour
+
+import org.apache.spark.AccumulatorParam
+
+/**
+ * A custom accumulator for string concatenation
+ * Contrived example -- see recipe for caveats.
+ */
+object StringAccumulatorParam extends AccumulatorParam[String] {
+    def zero(initialValue: String): String = {
+        ""
+    }
+                
+    def addInPlace(s1: String, s2: String): String = {
+        s1.trim + " " + s2.trim
+    }
+}
