62a29ac40eebdc8d76e44c1297324ef144ca1237
buri <buri@c041af27-2689-401b-8632-fcab6d716d80>
2016-09-20 21:22:00 +0000

    SPARKOUR-18: Updated generated ZIP for using-sql-udf

--- trunk/data/examples/sparkour/using-sql-udf/src/main/java/buri/sparkour/JUsingSqlUdf.java
+++ trunk/data/examples/sparkour/using-sql-udf/src/main/java/buri/sparkour/JUsingSqlUdf.java
@@ -19,11 +19,10 @@ package buri.sparkour;
 
 import static org.apache.spark.sql.functions.*;
 
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Column;
-import org.apache.spark.sql.DataFrame;
-import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.DataTypes;
 
 /**
@@ -33,58 +32,56 @@ import org.apache.spark.sql.types.DataTypes;
 public final class JUsingSqlUdf {
 
 	public static void main(String[] args) throws Exception {
-		SparkConf sparkConf = new SparkConf().setAppName("JUsingSqlUdf");
-		JavaSparkContext sc = new JavaSparkContext(sparkConf);
-		SQLContext sqlContext = new SQLContext(sc);
+		SparkSession spark = SparkSession.builder().appName("JUsingSqlUdf").getOrCreate();
 
 		// Create a DataFrame based on the JSON results.
-		DataFrame rawDF = sqlContext.read().json("loudoun_d_primary_results_2016.json");
+		Dataset<Row> rawDF = spark.read().json("loudoun_d_primary_results_2016.json");
 
 		// Register as a SQL-accessible table
-		rawDF.registerTempTable("votes");
+		rawDF.createOrReplaceTempView("votes");
 
 		System.out.println("Who was on the ballet?");
 		// Get all distinct candidate names from the DataFrame
 		String query = "SELECT DISTINCT candidate_name FROM votes";
-		sqlContext.sql(query).show();
+		spark.sql(query).show();
 
 		System.out.println("What order were candidates on the ballot?");
 		// Get the ballot order and discard the many duplicates (all VA ballots are the same)
 		// We also register this DataFrame as a table to reuse later.
 		query = "SELECT DISTINCT candidate_name, candidate_ballot_order "
 			+ "FROM votes ORDER BY candidate_ballot_order";
-		DataFrame orderDF = sqlContext.sql(query);
-		orderDF.registerTempTable("ordered_candidates");
+		Dataset<Row> orderDF = spark.sql(query);
+		orderDF.createOrReplaceTempView("ordered_candidates");
 		orderDF.show();
 
 		System.out.println("What order were candidates on the ballot (in descriptive terms)?");
 		// Load a reference table of friendly names for the ballot orders.
-		DataFrame friendlyDF = sqlContext.read().json("friendly_orders.json");
-		friendlyDF.registerTempTable("ballot_order");
+		Dataset<Row> friendlyDF = spark.read().json("friendly_orders.json");
+		friendlyDF.createOrReplaceTempView("ballot_order");
 		// Join the tables so the results show descriptive text
 		query = "SELECT oc.candidate_name, bo.friendly_name " 
 			+ "FROM ordered_candidates oc JOIN ballot_order bo "
 			+ "ON oc.candidate_ballot_order = bo.candidate_ballot_order";
-		sqlContext.sql(query).show();
+		spark.sql(query).show();
 
 		System.out.println("How many votes were cast?");
 		// Orginal data is string-based. Create a UDF to cast as an integer.
-		sqlContext.udf().register("to_int", (String x) -> Integer.valueOf(x), DataTypes.IntegerType);
+		spark.udf().register("to_int", (String x) -> Integer.valueOf(x), DataTypes.IntegerType);
 		query = "SELECT SUM(to_int(total_votes)) AS sum_total_votes FROM votes";
-		sqlContext.sql(query).show();
+		spark.sql(query).show();
 
 		System.out.println("How many votes did each candidate get?");
 		query = "SELECT candidate_name, SUM(to_int(total_votes)) AS sum_total_votes "
 			+ "FROM votes GROUP BY candidate_name ORDER BY sum_total_votes DESC";
-		sqlContext.sql(query).show();
+		spark.sql(query).show();
 
 		System.out.println("Which polling station had the highest physical turnout?");
 		// All physical precincts have a numeric code. Provisional/absentee precincts start with "##".
 		query = "SELECT precinct_name, SUM(to_int(total_votes)) AS sum_total_votes "
 			+ "FROM votes WHERE precinct_code NOT LIKE '##%' "
 			+ "GROUP BY precinct_name ORDER BY sum_total_votes DESC LIMIT 1";
-		sqlContext.sql(query).show();
+		spark.sql(query).show();
 
-		sc.stop();
+		spark.stop();
 	}
 }
--- trunk/data/examples/sparkour/using-sql-udf/src/main/r/using_sql_udf.R
+++ trunk/data/examples/sparkour/using-sql-udf/src/main/r/using_sql_udf.R
@@ -19,59 +19,58 @@
 # User-Defined Functions (UDFs).
 
 library(SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))
-sc <- sparkR.init()
-sqlContext <- sparkRSQL.init(sc)
+session <- sparkR.session()
 
 # Create a DataFrame based on the JSON results.
-rawDF <- read.df(sqlContext, "loudoun_d_primary_results_2016.json", "json")
+rawDF <- read.df("loudoun_d_primary_results_2016.json", "json")
 
 # Register as a SQL-accessible table
-registerTempTable(rawDF, "votes")
+createOrReplaceTempView(rawDF, "votes")
 
 print("Who was on the ballet?")
 # Get all distinct candidate names from the DataFrame
 query <- "SELECT DISTINCT candidate_name FROM votes"
-print(collect(sql(sqlContext, query)))
+print(collect(sql(query)))
 
 print("What order were candidates on the ballot?")
 # Get the ballot order and discard the many duplicates (all VA ballots are the same)
 # We also register this DataFrame as a table to reuse later.
 query <- paste("SELECT DISTINCT candidate_name, candidate_ballot_order",
     "FROM votes ORDER BY candidate_ballot_order", sep=" ")
-orderDF <- sql(sqlContext, query)
-registerTempTable(orderDF, "ordered_candidates")
+orderDF <- sql(query)
+createOrReplaceTempView(orderDF, "ordered_candidates")
 print(collect(orderDF))
 
 print("What order were candidates on the ballot (in descriptive terms)?")
 # Load a reference table of friendly names for the ballot orders.
-friendlyDF <- read.df(sqlContext, "friendly_orders.json", "json")
-registerTempTable(friendlyDF, "ballot_order")
+friendlyDF <- read.df("friendly_orders.json", "json")
+createOrReplaceTempView(friendlyDF, "ballot_order")
 # Join the tables so the results show descriptive text
 query <- paste("SELECT oc.candidate_name, bo.friendly_name",
     "FROM ordered_candidates oc JOIN ballot_order bo",
     "ON oc.candidate_ballot_order = bo.candidate_ballot_order", sep=" ")
-print(collect(sql(sqlContext, query)))
+print(collect(sql(query)))
 
 print("How many votes were cast?")
 # Orginal data is string-based. Create an integer version of the total votes column.
 # Because UDFs are not yet supported in SparkR, we cast the column first, then run SQL.
 votesColumn <- alias(cast(rawDF$total_votes, "int"), "total_votes_int")
 votesDF <- withColumn(rawDF, "total_votes_int", cast(rawDF$total_votes, "int"))
-registerTempTable(votesDF, "votes_int")
+createOrReplaceTempView(votesDF, "votes_int")
 # Get the integer-based votes column and sum all values together
 query <- "SELECT SUM(total_votes_int) AS sum_total_votes FROM votes_int"
-print(collect(sql(sqlContext, query)))
+print(collect(sql(query)))
 
 print("How many votes did each candidate get?")
 query <- paste("SELECT candidate_name, SUM(total_votes_int) AS sum_total_votes",
     "FROM votes_int GROUP BY candidate_name ORDER BY sum_total_votes DESC", sep=" ")
-print(collect(sql(sqlContext, query)))
+print(collect(sql(query)))
     
 print("Which polling station had the highest physical turnout?")
 # All physical precincts have a numeric code. Provisional/absentee precincts start with "##".
 query <- paste("SELECT precinct_name, SUM(total_votes_int) AS sum_total_votes",
     "FROM votes_int WHERE precinct_code NOT LIKE '##%'",
     "GROUP BY precinct_name ORDER BY sum_total_votes DESC LIMIT 1", sep=" ")
-print(collect(sql(sqlContext, query)))
+print(collect(sql(query)))
 
 sparkR.stop()
