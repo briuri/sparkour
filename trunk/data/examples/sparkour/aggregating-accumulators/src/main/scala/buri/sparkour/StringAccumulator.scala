c4b64ffee1564a295ced6580b92b2f4803635784
buri <buri@c041af27-2689-401b-8632-fcab6d716d80>
2016-09-21 11:17:36 +0000

    SPARKOUR-18: Fixed whitespace.

--- trunk/data/examples/sparkour/aggregating-accumulators/src/main/scala/buri/sparkour/StringAccumulator.scala
+++ trunk/data/examples/sparkour/aggregating-accumulators/src/main/scala/buri/sparkour/StringAccumulator.scala
@@ -28,31 +28,31 @@ import org.apache.spark.util.AccumulatorV2
  */
 class StringAccumulator(private var _value: String) extends AccumulatorV2[String, String] {
 
-    def this() {
-        this("")
-    }
+	def this() {
+		this("")
+	}
 
-    override def add(newValue: String): Unit = {
-        _value = value + " " + newValue.trim
-    }
+	override def add(newValue: String): Unit = {
+		_value = value + " " + newValue.trim
+	}
 
-    override def copy(): StringAccumulator = {
-        new StringAccumulator(value) 
-    }
+	override def copy(): StringAccumulator = {
+		new StringAccumulator(value) 
+	}
 
-    override def isZero(): Boolean = {
-        value.length() == 0
-    }
+	override def isZero(): Boolean = {
+		value.length() == 0
+	}
 
-    override def merge(other: AccumulatorV2[String, String]): Unit = {
-        add(other.value)
-    }
+	override def merge(other: AccumulatorV2[String, String]): Unit = {
+		add(other.value)
+	}
 
-    override def reset(): Unit = {
-        _value = ""
-    }
+	override def reset(): Unit = {
+		_value = ""
+	}
 
-    override def value(): String = {
-        _value
-    }
+	override def value(): String = {
+		_value
+	}
 }
